a9dc841a24eed1d50999191e98f61a7b6a5a3080
ACTS-HORIZON <42858845+ACTS-HORIZON@users.noreply.github.com>
2025-12-31 09:25:26 -0600

    Revert to 450 due to steering errors (likely lat_accel safety)

--- opendbc/safety/tests/test_hyundai_canfd.py
+++ opendbc/safety/tests/test_hyundai_canfd.py
@@ -30,7 +30,7 @@ class TestHyundaiCanfdBase(HyundaiButtonBase, common.CarSafetyTest, common.Drive
 
   MAX_RATE_UP = 3
   MAX_RATE_DOWN = 3
-  MAX_TORQUE_LOOKUP = [0], [500]
+  MAX_TORQUE_LOOKUP = [0], [450]
 
   MAX_RT_DELTA = 250
 
