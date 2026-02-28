905e679a755376efa9bd866df1d18fa7cabf0c39
ACTS-HORIZON <42858845+ACTS-HORIZON@users.noreply.github.com>
2026-02-27 19:28:34 -0600

    .04 actuator delay

--- opendbc/car/hyundai/interface.py
+++ opendbc/car/hyundai/interface.py
@@ -122,7 +122,7 @@ class CarInterface(CarInterfaceBase):
     # Common lateral control setup
 
     ret.centerToFront = ret.wheelbase * 0.4
-    ret.steerActuatorDelay = 0.055
+    ret.steerActuatorDelay = 0.04
     ret.steerLimitTimer = 0.4
     CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
 
