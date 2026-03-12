b7471d3fb907eba4bfd37c909e2140917ef97452
ACTS-HORIZON <42858845+ACTS-HORIZON@users.noreply.github.com>
2026-03-11 20:24:16 -0500

    Best fit copy of HDA2 damp ramp

--- opendbc/car/hyundai/hyundaicanfd.py
+++ opendbc/car/hyundai/hyundaicanfd.py
@@ -37,7 +37,7 @@ class CanBus(CanBusBase):
     return self._cam
 
 
-def create_steering_messages(packer, CP, CAN, enabled, lat_active, apply_torque, lkas_icon):
+def create_steering_messages(packer, CP, CAN, enabled, lat_active, apply_torque, lkas_icon, v_ego=0.):
   common_values = {
     "LKA_MODE": 2,
     "LKA_ICON": lkas_icon,
@@ -47,7 +47,7 @@ def create_steering_messages(packer, CP, CAN, enabled, lat_active, apply_torque,
     "STEER_MODE": 0,
     "HAS_LANE_SAFETY": 0,  # hide LKAS settings
     "NEW_SIGNAL_2": 0,
-    "DAMP_FACTOR": 200,  # can potentially tuned for better perf [3, 200]
+    "DAMP_FACTOR": int(np.interp(v_ego, [0, 50], [100, 170])),
   }
 
   lkas_values = copy.copy(common_values)
