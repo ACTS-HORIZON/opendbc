96d4e47275a2eeb51e9fe74408016fd2b7dcf024
ACTS-HORIZON <42858845+ACTS-HORIZON@users.noreply.github.com>
2026-06-10 22:51:43 -0500

    ,

--- opendbc/safety/modes/hyundai_canfd.h
+++ opendbc/safety/modes/hyundai_canfd.h
@@ -143,7 +143,7 @@ static void hyundai_canfd_rx_hook(const CANPacket_t *msg) {
 static bool hyundai_canfd_tx_hook(const CANPacket_t *msg) {
   const TorqueSteeringLimits HYUNDAI_CANFD_STEERING_LIMITS = {
     .max_torque = 409,
-    .max_rt_delta = 250
+    .max_rt_delta = 250,
     .max_rate_up = 3,
     .max_rate_down = 5,
     .driver_torque_allowance = 200,
